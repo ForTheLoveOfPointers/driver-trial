3680e77eedf67b4e011a73ac8b0a01e0faaca9cf
Costa Lucas <lucas.costa@stackzone.com>
2025-01-24 15:53:47 -0300

    Driver basics done

--- KM Driver/Source/main.cpp
+++ KM Driver/Source/main.cpp
@@ -14,7 +14,6 @@ void debug_print(PCSTR text) {
 	UNREFERENCED_PARAMETER(text);
 #endif // !DEBUG
 
-
 	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL,text));
 }
 
@@ -28,8 +27,10 @@ namespace driver {
 	struct Request
 	{
 		HANDLE process_id;
+
 		PVOID target;
 		PVOID buffer;
+		
 		SIZE_T size;
 		SIZE_T return_size;
 	};
@@ -45,12 +46,50 @@ namespace driver {
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
 		return irp->IoStatus.Status;
 	}
-
-	// TODO: complete this function
+	
 	NTSTATUS device_control(PDEVICE_OBJECT device_object, PIRP irp) {
 		UNREFERENCED_PARAMETER(device_object);
+		debug_print("[+] Device control called");
+
+		NTSTATUS status = STATUS_UNSUCCESSFUL;
+		
+		PIO_STACK_LOCATION stack_irp = IoGetCurrentIrpStackLocation(irp);
+		auto request = reinterpret_cast<Request*>(irp->AssociatedIrp.SystemBuffer);
+
+		if (stack_irp == nullptr || request == nullptr) {
+			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			return status;
+		}
+
+		static PEPROCESS target_process = nullptr;
+		const ULONG control_code = stack_irp->Parameters.DeviceIoControl.IoControlCode;
+
+		switch (control_code)
+		{
+		case codes::attach:
+			status = PsLookupProcessByProcessId(request->process_id, &target_process);
+			break;
+		case codes::read:
+			if(target_process != nullptr) { 
+				status = MmCopyVirtualMemory(target_process, request->target, PsGetCurrentProcess(),
+					request->buffer, request->size, KernelMode, &request->return_size);
+			}
+			break;
+		case codes::write:
+			if (target_process != nullptr) {
+				status = MmCopyVirtualMemory(PsGetCurrentProcess(), request->buffer, target_process,
+					request->target, request->size, KernelMode, &request->return_size);
+			}
+			break;
+		default:
+			break;
+		}
+		
+		irp->IoStatus.Status = status;
+		irp->IoStatus.Information = sizeof(Request);
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
-		return irp->IoStatus.Status;
+
+		return status;
 	}
 }
 
